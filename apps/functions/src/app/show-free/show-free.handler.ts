366f9486ac0b31c49ca532e5539a9634d7115bdd
Jakub Wilczewski <jwilczewski@consdata.pl>
2020-08-24 20:17:27 +0200

    zmiana na buttony

--- apps/functions/src/app/show-free/show-free.handler.ts
+++ apps/functions/src/app/show-free/show-free.handler.ts
@@ -1,5 +1,6 @@
 import {ReserveDeskMessage} from '../reserve-desk.message';
 import * as nodeFetch from 'node-fetch';
+import {DockStation} from './dock-station';
 
 async function sendSlackMessage(slackHttpHeaders: { Authorization: string; 'Content-type': string }, channel: string, message: string) {
   await nodeFetch(`https://slack.com/api/chat.postMessage`, {
@@ -14,32 +15,146 @@ async function sendSlackMessage(slackHttpHeaders: { Authorization: string; 'Cont
 
 const roomsDefinitions = [
   {
-    name: 'sharki',
-    desks: ['b1', 'b2', 'b3', 'b4', 'b5']
+    name: 'Sharki',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215391975',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-3',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-4',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-5',
+        dockStation: DockStation.old
+      }
+    ]
   },
   {
-    name: 'piranie',
-    desks: ['b1', 'b2', 'b3', 'b4']
+    name: 'Piranie',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392024',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-3',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-4',
+        dockStation: DockStation.new
+      }
+    ]
   },
   {
-    name: 'lemury',
-    desks: ['b1', 'b2', 'b3', 'b4'],
+    name: 'Lemury',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392036',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-3',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-4',
+        dockStation: DockStation.old
+      }
+    ]
   },
   {
-    name: 'komando',
-    desks: ['b1', 'b2', 'b3', 'b4', 'b5'],
+    name: 'Komando',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392057',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.new
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-3',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-4',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-5',
+        dockStation: DockStation.old
+      }
+    ]
   },
   {
-    name: 'orki',
-    desks: ['b1', 'b2'],
+    name: 'Orki',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392071',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.new
+      }
+    ]
   },
   {
-    name: 'magicy',
-    desks: ['b1', 'b2', 'b3'],
+    name: 'Magicy',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392110',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-3',
+        dockStation: DockStation.new
+      }
+    ]
   },
   {
-    name: 'osy',
-    desks: ['b1', 'b2']
+    name: 'Osy',
+    wikiLink: 'https://wiki.consdata.pl/pages/viewpage.action?pageId=215392129',
+    desks: [
+      {
+        name: 'B-1',
+        dockStation: DockStation.old
+      },
+      {
+        name: 'B-2',
+        dockStation: DockStation.unknown
+      }
+    ]
   }
 ];
 
